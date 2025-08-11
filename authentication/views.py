a336f4d995f65ca0c902990c3e2269c0b2cd04f9
agyesam12 <agyesam12@gmail.com>
2025-08-11 11:22:30 +0000

    added Logout View

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 from django.views. generic import View
+from packages.log_entry import create_log_entry
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.admin.models import LogEntry
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib.auth import login
+from django.contrib import messages
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
+from .models import User
+from django.db.models import Q, Count, Avg, Sum
+from django.utils import timezone
+from datetime import datetime, timedelta
+from collections import defaultdict
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -12,3 +28,25 @@ class RegisterView(View):
     def post(self, request, *args, **kwargs):
         # Handle registration logic here
         pass
+
+
+
+
+class LogoutView(View):
+    def get(self, request):
+        if request.user.is_authenticated:
+            messages.success(request, "You have been successfully logged out. See you soon!")
+            create_log_entry(
+                user=request.user,
+                content_type=ContentType.objects.get_for_model(User),
+                object_id=request.user.pk,
+                object_repr=str(request.user),
+                action_flag=2,  # CHANGE action
+                change_message=f"User {request.user.full_name} has logged out successfully"
+            )
+            logout(request)
+            
+
+            return redirect('login')
+        else:
+            return redirect('home')

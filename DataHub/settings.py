6786abe564ab766a2740d4cee52b0d0c89368fc0
agyesam12 <agyesam12@gmail.com>
2025-08-11 12:11:41 +0000

    configured email setups

--- DataHub/settings.py
+++ DataHub/settings.py
@@ -190,3 +190,23 @@ LOGGING = {
         },
     }
 }
+
+
+if DEBUG:
+    # Development settings
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  
+EMAIL_PORT = 587             
+EMAIL_USE_TLS = True         
+EMAIL_HOST_USER = 'your_email@gmail.com'  
+EMAIL_HOST_PASSWORD = 'your_app_password' 
+DEFAULT_FROM_EMAIL = 'DataHub <no-reply@yourdomain.com>'
+
+# Additional security measures
+# You can use environment variables to keep sensitive information secure.
+# For example:
+# import os
+# EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')

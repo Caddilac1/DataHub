95d9e708f7273a183195c2b3a8f80653f0ecab4a
agyesam12 <agyesam12@gmail.com>
2025-08-11 09:43:58 +0000

    added urls.py file for authentication

---
+++ authentication/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.contrib.auth.views import LogoutView
+from django.conf.urls.static import static
+from .views import *
+
+urlpatterns = [
+   path('', HomeView, name='home'),
+   path('register/', CustomerRegisterView.as_view(), name='register'),
+   path('login/', UserLoginView.as_view(), name='login'),
+   path('logout/', UserLogoutView.as_view(), name='logout'),
+]

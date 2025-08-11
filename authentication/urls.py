b266a086f6044a125053ac4fbce4f0f92e7a0d7d
agyesam12 <agyesam12@gmail.com>
2025-08-11 10:24:27 +0000

    added authentication urls path

--- authentication/urls.py
+++ authentication/urls.py
@@ -8,7 +8,5 @@ from system.views import *
 
 urlpatterns = [
    path('', HomeView, name='home'),
-   path('register/', CustomerRegisterView.as_view(), name='register'),
-   path('login/', UserLoginView.as_view(), name='login'),
-   path('logout/', UserLogoutView.as_view(), name='logout'),
+   
 ]

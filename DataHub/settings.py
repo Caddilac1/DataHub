5f8568f092886cfe6eb8da0791a02175f04c17ce
agyesam12 <agyesam12@gmail.com>
2025-08-24 15:52:58 +0000

    upated settings to test

--- DataHub/settings.py
+++ DataHub/settings.py
@@ -52,8 +52,8 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'allauth',
     'allauth.account',
-    'allauth.socialaccount',
-    'allauth.socialaccount.providers.google',
+    #'allauth.socialaccount',
+    #'allauth.socialaccount.providers.google',
     'authentication',
     'storages',
 ]

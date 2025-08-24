e1e9841d86f1a97c2dd9e2cda9ecc54d4fd85a54
agyesam12 <agyesam12@gmail.com>
2025-08-24 12:10:19 +0000

    updated the settings.py file to configure secret keys

--- DataHub/settings.py
+++ DataHub/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # Quick-start development settings - unsuitable for production

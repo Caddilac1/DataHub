adae4a3c955887519351e67597463275f65e2a35
agyesam12 <agyesam12@gmail.com>
2025-08-25 19:25:23 +0000

    added Admin view orders urls path

--- management/urls.py
+++ management/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import AdminHomePageView, AdminDashboardView, AdminViewAllUsersView
+from .views import AdminHomePageView, AdminDashboardView, AdminViewAllUsersView, AdminviewAllOrders
 
 
 urlpatterns = [
     path('admin_home_page/', AdminHomePageView.as_view(), name='admin_home_page'),
     path('admin_dashboard/', AdminDashboardView.as_view(), name='admin_dashboard'),
     path('view/users/', AdminViewAllUsersView.as_view(), name='view_all_users'),
+    path('orders/all_orders/', AdminviewAllOrders.as_view(), name='view_all_orders'),
 ]

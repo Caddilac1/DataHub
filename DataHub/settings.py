083d30f6698a20a8a57917250076d21d6fd82924
Caddilac1 <155891049+Caddilac1@users.noreply.github.com>
2025-08-25 10:17:41 +0000

    fixed css conflict issue

--- DataHub/settings.py
+++ DataHub/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1','datamart.up.railway.app','datahub.up.railway.app']
 SITE_ID = 1
